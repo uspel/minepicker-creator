233a5d89a6549d285d7ed7a6446fbb796cb6cff0
QuazChick <quazchicken@outlook.com>
2024-08-06 18:14:25 +0100

    Defined yargs scriptName

--- src/utils/package/registerCommands.ts
+++ src/utils/package/registerCommands.ts
@@ -3,7 +3,7 @@ import { hideBin } from "yargs/helpers";
 
 import commands from "../../commands";
 
-const argv = yargs(hideBin(process.argv)).usage("Usage: minepicker <command> [options]");
+const argv = yargs(hideBin(process.argv)).scriptName("minepicker").usage("Usage: minepicker <command> [options]");
 
 for (const command of commands) argv.command(command);
 
